b322d586da7ed4aecbbc274715bb72742018f5c8
jineshpatel2002 <jineshpatel2002@gmail.com>
2024-07-10 15:29:44 +0530

    feat(app): add route constants

---
+++ src/constants/route.constant.ts
@@ -0,0 +1,7 @@
+export const routes = {
+    HOME_ENDPOINT: "/",
+    GAME_ENDPOINT: "/api/game",
+    GAME_INIT_ENDPOINT: "/initialize",
+    PLAY_ROUND_ENDPOINT: "/play-round",
+    STATUS_ENDPOINT: "/status",
+}
--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -1,16 +1,18 @@
 // PKGS
 import { Router } from 'express';
+
+// IMPORTS
+import { routes } from "../constants/route.constant";
+
 // CONTROLLERS
 import { GameController } from '../controllers/game.controller';
-// MIDDLEWARES
-import { handleResponseStatus } from '../middlewares/statusHandler.middleware';
 
 // ROUTER INITIALIZATION
 const router = Router();
 
 // ROUTES
-router.post('/initialize', GameController.initializeGame);
-router.post('/play-round', GameController.playRound);
-router.get('/status', GameController.getStatus);
+router.post(routes.GAME_INIT_ENDPOINT, GameController.initializeGame);
+router.post(routes.PLAY_ROUND_ENDPOINT, GameController.playRound);
+router.get(routes.STATUS_ENDPOINT,  GameController.getStatus);
 
 export default router;
