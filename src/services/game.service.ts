c3cc8c7129e2654fe06de326b2ef71021fc7b3c9
jineshpatel2002 <jineshpatel2002@gmail.com>
2024-07-10 14:35:40 +0530

    feat(app):create game service with game logic

---
+++ src/services/game.service.ts
@@ -0,0 +1,41 @@
+import { Player } from '../models/player.model';
+import { IPlayer } from '../interfaces/player.interface';
+import { diceRoller } from '../utils/diceRoller';
+
+export class GameService {
+  private player1: Player;
+  private player2: Player;
+
+  public initializeGame(player1IFC: IPlayer, player2IFC: IPlayer): { player1: Player, player2: Player } {
+    this.player1 = new Player(player1IFC.health, player1IFC.strength, player1IFC.attack, player1IFC.name);
+    this.player2 = new Player(player2IFC.health, player2IFC.strength, player2IFC.attack, player2IFC.name);
+    return { player1: this.player1, player2: this.player2 };
+  }
+
+  public playRound(attackerName: string): { message: string, attacker: Player, defender: Player, defenderHealth: number } {
+    const attacker = this.player1.name === attackerName ? this.player1 : this.player2;
+    const defender = attacker === this.player1 ? this.player2 : this.player1;
+
+    const attackRoll = diceRoller.roll();
+    const defendRoll = diceRoller.roll();
+
+    const attackDamage = attacker.attack * attackRoll;
+    const defendDamage = defender.strength * defendRoll;
+
+    const damageDealt = attackDamage - defendDamage;
+    if (damageDealt > 0) {
+      defender.health -= damageDealt;
+    }
+
+    return {
+      message: 'Round played',
+      attacker,
+      defender,
+      defenderHealth: defender.health,
+    };
+  }
+
+  public getStatus(): { player1: Player, player2: Player } {
+    return { player1: this.player1, player2: this.player2 };
+  }
+}
