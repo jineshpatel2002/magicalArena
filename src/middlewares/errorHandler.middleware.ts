cfe9151febd5fe44fc704671e51e4fe53c45bcd7
jineshpatel2002 <jineshpatel2002@gmail.com>
2024-07-10 15:00:38 +0530

    refactor(middleware): update error response

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -3,5 +3,10 @@ import { StatusCodes } from "http-status-codes";
 
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.message });
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ 
+    message: err.message,
+    data: {
+        data: [],
+    },
+  });
 }
